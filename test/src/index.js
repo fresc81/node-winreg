c487a20909cff081a22e2d326514368f9ff8bc1f
Paul Bottin <paulbottin+git@gmail.com>
2016-04-17 03:22:03 +0200

    * added test cases for all methods

--- test/src/index.js
+++ test/src/index.js
@@ -1,12 +1,12 @@
 
-// this should reveal wrong iteration used for arrays
+// this should reveal wrong iteration on arrays
 Array.prototype.someNewMethod = function() {};
 
 var test  = require('unit.js');
 
 describe('winreg', function(){
   
-  it('we\'re running on Windows', function () {
+  it('running on Windows', function () {
     
     test.string(process.platform)
     . is('win32');
@@ -23,7 +23,7 @@ describe('winreg', function(){
     
   });
   
-  // create a uniqe registry key in HKCU to test in...
+  // create a uniqe registry key in HKCU to test in
   var regKey = new Registry({
     hive: Registry.HKCU,
     key:  '\\Software\\AAA_' + new Date().toISOString()
@@ -36,11 +36,24 @@ describe('winreg', function(){
     
   });
   
+  // a key that has subkeys in it
+  var softwareKey = new Registry({
+    hive: Registry.HKCU,
+    key:  '\\Software'
+  });
+  
+  it('softwareKey is instance of Registry', function(){
+    
+    test.object(softwareKey)
+    . isInstanceOf(Registry);
+    
+  });
+  
   describe('Registry', function (){
     
     describe('keyExists()', function(){
       
-      it('regKey has method', function () {
+      it('regKey has keyExists method', function () {
         
         test.object(regKey)
         . hasProperty('keyExists');
@@ -69,7 +82,7 @@ describe('winreg', function(){
     
     describe('create()', function(){
       
-      it('regKey has method', function () {
+      it('regKey has create method', function () {
         
         test.object(regKey)
         . hasProperty('create');
@@ -108,65 +121,9 @@ describe('winreg', function(){
       
     }); // end - describe create()
     
-    describe('keys()', function (){
-      
-      it('regKey has method', function () {
-        
-        test.object(regKey)
-        . hasProperty('keys');
-        
-        test.function(regKey.keys)
-        . hasName('keys');
-        
-      });
-      
-    }); // end - describe keys()
-    
-    describe('valueExists()', function (){
-      
-      it('regKey has method', function () {
-        
-        test.object(regKey)
-        . hasProperty('valueExists');
-        
-        test.function(regKey.valueExists)
-        . hasName('valueExists');
-        
-      });
-      
-    }); // end - describe valueExists
-    
-    describe('get()', function (){
-      
-      it('regKey has method', function () {
-        
-        test.object(regKey)
-        . hasProperty('get');
-        
-        test.function(regKey.get)
-        . hasName('get');
-        
-      });
-      
-    }); // end - describe get
-    
-    describe('values()', function (){
-      
-      it('regKey has method', function () {
-        
-        test.object(regKey)
-        . hasProperty('values');
-        
-        test.function(regKey.values)
-        . hasName('values');
-        
-      });
-      
-    }); // end - describe values
-    
     describe('set()', function (){
       
-      it('regKey has method', function () {
+      it('regKey has set method', function () {
         
         test.object(regKey)
         . hasProperty('set');
@@ -176,11 +133,115 @@ describe('winreg', function(){
         
       });
       
+      it('can set a string value', function (done) {
+        
+        regKey.set('SomeString', Registry.REG_SZ, 'SomeValue', function (err) {
+          
+          if (err) throw err;
+          
+          done();
+          
+        });
+        
+      });
+      
     }); // end - describe set
+    
+    describe('valueExists()', function (){
+      
+      it('regKey has valueExists method', function () {
+        
+        test.object(regKey)
+        . hasProperty('valueExists');
+        
+        test.function(regKey.valueExists)
+        . hasName('valueExists');
+        
+      });
+      
+      it('can check for existing string value', function (done) {
+        
+        regKey.valueExists('SomeString', function (err, exists) {
+          
+          if (err) throw err;
+          
+          test.bool(exists)
+          . isTrue();
+          
+          done();
+          
+        });
+        
+      });
+      
+    }); // end - describe valueExists
+    
+    describe('get()', function (){
+      
+      it('regKey has get method', function () {
+        
+        test.object(regKey)
+        . hasProperty('get');
+        
+        test.function(regKey.get)
+        . hasName('get');
+        
+      });
+      
+      it('can get a string value', function (done) {
+        
+        regKey.get('SomeString', function (err, item) {
+          
+          if (err) throw err;
+          
+          test.object(item)
+          . hasProperty('value', 'SomeValue');
+          
+          done();
+          
+        });
+        
+      });
+      
+    }); // end - describe get
+    
+    describe('values()', function (){
+      
+      it('regKey has values method', function () {
+        
+        test.object(regKey)
+        . hasProperty('values');
+        
+        test.function(regKey.values)
+        . hasName('values');
+        
+      });
+      
+      it('returns array of RegistryItem objects', function (done) {
+        
+        regKey.values(function (err, items) {
+          
+          if (err) throw err;
+          
+          for (var i=0; i<items.length; i++) {
+            
+            var item = items[i];
+            
+            test.object(item)
+            . hasProperty('value');
+            
+          }
+          done();
+          
+        });
+        
+      });
+      
+    }); // end - describe values
     
     describe('remove()', function (){
       
-      it('regKey has method', function () {
+      it('regKey has remove method', function () {
         
         test.object(regKey)
         . hasProperty('remove');
@@ -190,11 +251,60 @@ describe('winreg', function(){
         
       });
       
+      it('can remove a string value', function (done) {
+        
+        regKey.remove('SomeString', function (err) {
+          
+          if (err) throw err;
+          
+          done();
+          
+        });
+        
+      });
+      
     }); // end - describe remove
+    
+    describe('keys()', function (){
+      
+      it('regKey has keys method', function () {
+        
+        test.object(regKey)
+        . hasProperty('keys');
+        
+        test.function(regKey.keys)
+        . hasName('keys');
+        
+      });
+      
+      it('returns array of Registry objects', function (done) {
+        
+        softwareKey.keys(function (err, keys) {
+          
+          if (err) throw err;
+          
+          test.array(keys);
+          
+          for (var i=0; i<keys.length; i++) {
+            
+            var key = keys[i];
+            
+            test.object(key)
+            . isInstanceOf(Registry);
+            
+          }
+          
+          done();
+          
+        });
+        
+      });
+      
+    }); // end - describe keys()
     
     describe('clear()', function (){
       
-      it('regKey has method', function () {
+      it('regKey has clear method', function () {
         
         test.object(regKey)
         . hasProperty('clear');
@@ -208,7 +318,7 @@ describe('winreg', function(){
     
     describe('destroy()', function(){
       
-      it('regKey has method', function () {
+      it('regKey has destroy method', function () {
         
         test.object(regKey)
         . hasProperty('destroy');
